e732701298c85ed274783812bcd39bcf2ba9d2ba
Robert Chubb <paraboliclabs@gmail.com>
2019-01-20 14:31:22 -0500

    Init

---
+++ Source/SCPRedux/SCPReduxGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SCPReduxGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SCPREDUX_API ASCPReduxGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
